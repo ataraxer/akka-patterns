6d5dcaca47e8bfbb95bfbe28dbfa923678e1e165
Anton Karamanov <ataraxer@gmail.com>
2014-03-24 11:08:56 +0400

    Move FutureInt constructors to companion object

--- src/main/scala/futures/FutureInt.scala
+++ src/main/scala/futures/FutureInt.scala
@@ -5,17 +5,21 @@ import scala.concurrent.duration._
 
 
 object FutureInt {
-  implicit def futureToFutureInt(future: Future[Int]): FutureInt =
-    FutureInt(future)
+  implicit def futureToFutureInt(future: Future[Int])(implicit ec: ExecutionContext): FutureInt =
+    new FutureInt(future)
 
   implicit def futureIntToFuture(futureInt: FutureInt): Future[Int] =
     futureInt.future
+
+  def apply(body: => Int)(implicit ec: ExecutionContext): FutureInt =
+    new FutureInt( Future { body } )
+
+  def apply(future: Future[Int])(implicit ec: ExecutionContext): FutureInt =
+    new FutureInt(future)
 }
 
-case class FutureInt(future: Future[Int]) {
-  implicit val ex: ExecutionContext = ExecutionContext.global
-
-  def +(that: Future[Int]) =
+class FutureInt(val future: Future[Int])(implicit ex: ExecutionContext) {
+  def +(that: FutureInt) =
     for (a <- future; b <- that)
       yield a + b
 }
@@ -29,7 +33,12 @@ object FutureIntApp extends Application {
   val futureA = Future { 42 }
   val futureB = Future { 9000 }
 
+  futureA.onComplete(println)
+  futureB.onComplete(println)
+
   val futureC = futureA + futureB
+
+  futureC.onComplete(println)
 
   val result = Await.result(futureC, 1 second)
   println(result)
