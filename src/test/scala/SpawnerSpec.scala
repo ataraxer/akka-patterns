a48349302cde13bb45b70a722c53875cdde49e5b
Anton Karamanov <ataraxer@gmail.com>
2014-04-03 23:23:57 +0400

    Add Spawner spec

---
+++ src/test/scala/SpawnerSpec.scala
@@ -0,0 +1,62 @@
+package com.ataraxer.patterns.test.akka
+
+import org.scalatest._
+
+import akka.testkit.{TestKit, TestActorRef, ImplicitSender}
+import akka.actor.{ActorSystem, Actor, ActorRef}
+import akka.util.Timeout
+
+import com.ataraxer.patterns.akka.Spawner
+
+import scala.concurrent.duration._
+
+
+object SpawnerSpec {
+  case class Work
+  case class Ping
+  case class Done
+
+  class SpawnerActor extends Actor with Spawner {
+    val echoer = spawn(new Actor {
+      def receive = {
+        case msg => sender ! msg
+      }
+    })
+
+    def receive = {
+      case Work => {
+        val client = sender
+
+        val handler = handle {
+          case Ping => client ! Done
+        }
+
+        echoer.tell(Ping, handler)
+      }
+    }
+  }
+}
+
+
+class SpawnerSpec(_system: ActorSystem)
+    extends TestKit(_system)
+    with ImplicitSender
+    with UnitSpec
+{
+  import SpawnerSpec._
+
+  val spawner = TestActorRef[SpawnerActor]
+
+  def this() = this(ActorSystem("spawner-spec"))
+
+  "A Spawner" should "be able to spawn anonymous actors" in {
+    spawner.underlyingActor.echoer shouldBe an[ActorRef]
+  }
+
+  it should "be able to spawn anonymous handlers" in {
+    spawner ! Work
+    expectMsg(Done)
+  }
+}
+
+// vim: set ts=2 sw=2 et:
